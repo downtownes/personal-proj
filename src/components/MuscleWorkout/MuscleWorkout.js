f9fd82a9b3e9410e77c584139604c446ea43736e
unknown <townes.falcon@gmail.com>
2018-01-18 16:55:39 -0700

    inital commit

--- src/components/MuscleWorkout/MuscleWorkout.js
+++ src/components/MuscleWorkout/MuscleWorkout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export default class MuscleWorkout extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             user: []
@@ -12,23 +12,26 @@ export default class MuscleWorkout extends Component {
     }
 
 
-getWorkout() {
-    axios.get('/api/muscle').then(res => {
-        console.log(res.data)
-        this.setState({user: res.data})
-    })
-}
+    getWorkout() {
+        axios.get('/api/muscle').then(res => {
+            console.log(res.data)
+            this.setState({ user: res.data })
+        })
+    }
 
     render() {
-        let muscWorkout = this.state.user.map( (val, i) => {
+        let muscWorkout = this.state.user.map((val, i) => {
             return <h4 key={i}>{JSON.stringify(val)}</h4>
         })
-        return(
+        return (
             <div className="muscle-workout">
                 <h1>8 Week Muscle Builder</h1>
                 <div>
-                    <button onClick={ () => this.getWorkout()}>Get User</button>
+                    <button onClick={() => this.getWorkout()}>Get User</button>
                     {muscWorkout}
+                </div>
+                <div>
+                    
                 </div>
             </div>
         )
